c7f46c63ac89d8e3e561c6a00e0dde7a42bd577b
Vladislav Rychkov <vvrych3@gmail.com>
2022-10-29 21:37:03 +0600

    feat: add open and close function for modal

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+"use strict";
+
+const gulp = require("gulp");
+const webpack = require("webpack-stream");
+const browsersync = require("browser-sync");
+//const sass = require('gulp-sass')(require('sass'));
+
+const dist = "./dist/"; //выходной путь
+
+gulp.task("copy-html", () => {
+    return gulp.src("./src/index.html")
+                .pipe(gulp.dest(dist))
+                .pipe(browsersync.stream());
+}); //отслеживает изменения, которые вносим в html файл
+
+gulp.task("build-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'development',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    watch: false,
+                    devtool: "source-map",
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    debug: true,
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest(dist))
+                .on("end", browsersync.reload);
+}); //работа со скриптами - включает и вебпак
+
+gulp.task("copy-assets", () => {
+    return gulp.src("./src/assets/**/*.*")
+                .pipe(gulp.dest(dist + "/assets"))
+                .on("end", browsersync.reload);
+});
+
+gulp.task("watch", () => {
+    browsersync.init({
+		server: "./dist/",
+		port: 4000,
+		notify: true
+    });
+    
+    gulp.watch("./src/index.html", gulp.parallel("copy-html"));
+    gulp.watch("./src/assets/**/*.*", gulp.parallel("copy-assets"));
+    gulp.watch("./src/js/**/*.js", gulp.parallel("build-js"));
+}); //запускается отдельный сервер для отслеживания изменения файлов
+
+gulp.task("build", gulp.parallel("copy-html", "copy-assets", "build-js"));
+//билдит всё
+gulp.task("build-prod-js", () => {
+    return gulp.src("./src/js/main.js")
+                .pipe(webpack({
+                    mode: 'production',
+                    output: {
+                        filename: 'script.js'
+                    },
+                    module: {
+                        rules: [
+                          {
+                            test: /\.m?js$/,
+                            exclude: /(node_modules|bower_components)/,
+                            use: {
+                              loader: 'babel-loader',
+                              options: {
+                                presets: [['@babel/preset-env', {
+                                    corejs: 3,
+                                    useBuiltIns: "usage"
+                                }]]
+                              }
+                            }
+                          }
+                        ]
+                      }
+                }))
+                .pipe(gulp.dest(dist));
+}); //чистовой вариант компиляции скриптов
+
+gulp.task("default", gulp.parallel("watch", "build")); //запускает всё
---
+++ src/js/modules/modals.js
@@ -0,0 +1,54 @@
+const modals = () => {
+    //создать функцию для реализации 1 и 2 пункта ТЗ
+    function modal(openClass, modalClass, closeClass) {
+        //сначала получить элементы со страницы
+        const open = document.querySelectorAll(openClass);
+        const modal = document.querySelector(modalClass);
+        const close = document.querySelector(closeClass);
+
+        
+        
+        open.forEach(item => {
+            //для элементов, которые ссылки(а не кнопки - отключить стандартное поведение) - if(e.target) через него 
+            item.addEventListener('click', (e) => {
+                if(e.target) {
+                    e.preventDefault();
+                }
+                modal.style.display = 'block';
+                //чтоб не листалось всё для бади сделать стайл оверфлоу в хайдн
+                document.body.style.overflow = 'hidden';
+            });
+        });
+        
+        close.addEventListener('click', () => {
+            modal.style.display = 'none';
+            document.body.style.overflow = '';
+        });
+
+        modal.addEventListener('click', (e) => {
+            //тут как раз определяется подложка, а само модальное окно уже будет то, что внутри
+            if(e.target === modal) {
+                modal.style.display = 'none';
+                document.body.style.overflow = '';
+            }
+        });
+    
+    }
+
+    function showModalAfterOpen(selector, time) {
+        setTimeout(() => {
+            const module = document.querySelector(selector);
+            module.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }, time);
+    }
+
+    //1 пункт
+    modal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    //2 пункт
+    modal('.phone_link', '.popup', '.popup_close');
+    //10 пункт
+    showModalAfterOpen('.popup', 6000);
+};
+
+export default modals;
